4811ee205befd2bd633c142913f6771a116dce08
devJiraynor <jiraynor@naver.com>
2023-01-04 13:03:18 +0900

    2023.01.03. 12:50

--- Backend/Example/src/controller/MainController.java
+++ Backend/Example/src/controller/MainController.java
@@ -6,8 +6,19 @@ public class MainController {
 	
 	private MainService mainService;
 	
+	// MainContoroller class는 MainService class에 의존
 	public MainController() {
 		this.mainService = new MainService();
+	}
+	
+	// 1. Constructor(생성자)를 이용한 DI 구현
+	public MainController(MainService mainService) {
+		this.mainService = mainService;
+	}
+	
+	// 2. Setter 메서드를 이용한 DI 구현
+	public void setMainService(MainService mainService) {
+		this.mainService = mainService;
 	}
 	
 	public void hello() {
@@ -15,3 +26,9 @@ public class MainController {
 	}
 
 }
+
+
+
+
+
+
--- Backend/Example/src/depency/Main.java
+++ Backend/Example/src/depency/Main.java
@@ -1,12 +1,28 @@
 package depency;
 
 import controller.MainController;
+import service.MainService;
 
 public class Main {
 
 	public static void main(String[] args) {
+		// DI가 적용되지 않은 MainController
 		MainController mainController = new MainController();
 		mainController.hello();
+
+		MainService mainService = new MainService();
+		// Constructor를 이용하여 DI가 구현된 MainController
+		MainController mainControllerDi1 = new MainController(mainService);
+		
+		// Setter 메서드를 이용하여 DI가 구현된 MainController
+		MainController mainControllerDi2 = new MainController();
+		mainControllerDi2.setMainService(mainService);
 	}
 
 }
+
+
+
+
+
+
